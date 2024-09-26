de24ed46f1591bd4180720cea9e3f4834d565912
HorusMaster <ramswi@gmail.com>
2024-09-26 20:38:30 +0000

    made async

--- cat_control/main.py
+++ cat_control/main.py
@@ -1,5 +1,6 @@
 import sys
 import time
+import asyncio
 
 sys.path.append("/usr/lib/python3/dist-packages")
 
@@ -8,40 +9,47 @@ from adafruit_servokit import ServoKit
 kit = ServoKit(channels=16, address=0x40)
 
 
-def move_servo_with_delay(servo_channel, target_angle, current_angle):
+async def move_servo_with_delay(servo_channel, target_angle, current_angle):
     # Calcula la diferencia de ángulo
     angle_difference = abs(target_angle - current_angle)
 
     # Calcula el tiempo de espera en base a la velocidad del servo MG995
-    time_to_wait = (angle_difference / 60) * 0.22  # 0.2 segundos por 60 grados
+    time_to_wait = (angle_difference / 60) * 0.22  # 0.22 segundos por 60 grados
 
     # Mueve el servo al ángulo objetivo
     kit.servo[servo_channel].angle = target_angle
 
-    # Espera el tiempo calculado
-    time.sleep(time_to_wait)
+    # Espera el tiempo calculado de manera asíncrona
+    await asyncio.sleep(time_to_wait)
 
     print(
         f"Servo {servo_channel} movido de {current_angle}° a {target_angle}° en {time_to_wait:.2f} segundos"
     )
 
 
-# Rutina para hacer girar el servo de 0° a 180° 5 veces
-def routine_servo_5_cycles(servo_channel):
+async def routine_servo_5_cycles(servo_channel):
     current_angle = 0
     kit.servo[servo_channel].angle = 0
-    time.sleep(1)
+    await asyncio.sleep(1)
     for _ in range(5):
         # Mover de 0° a 180°
         target_angle = 180
-        move_servo_with_delay(servo_channel, target_angle, current_angle)
+        await move_servo_with_delay(servo_channel, target_angle, current_angle)
         current_angle = target_angle
 
         # Mover de 180° a 0°
         target_angle = 0
-        move_servo_with_delay(servo_channel, target_angle, current_angle)
+        await move_servo_with_delay(servo_channel, target_angle, current_angle)
         current_angle = target_angle
 
 
-# Ejecuta la rutina para el canal 8
-routine_servo_5_cycles(0)
+async def main():
+    # Ejecutar las rutinas de dos servos al mismo tiempo usando asyncio.gather
+    await asyncio.gather(
+        routine_servo_5_cycles(0),  # Mover el servo del canal 0
+        routine_servo_5_cycles(1),  # Mover el servo del canal 1
+    )
+
+
+# Ejecutar la función principal con asyncio
+asyncio.run(main())
