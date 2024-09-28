3f3a8dfed688c13c01241f1e41bfba9068bb8f3f
HorusMaster <ramswi@gmail.com>
2024-09-28 17:44:30 +0000

    f

--- cat_video/main.py
+++ cat_video/main.py
@@ -1,6 +1,6 @@
 import torch
 
-model = torch.hub.load(".", "custom","models/yolov5n-face.pt", source='local', force_reload=True)
+model = torch.hub.load("/code/yolov5", "custom", "models/yolov5n-face.pt", source='local', force_reload=True)
 
 img = "https://ultralytics.com/images/zidane.jpg"  # or file, Path, PIL, OpenCV, numpy, list
 
