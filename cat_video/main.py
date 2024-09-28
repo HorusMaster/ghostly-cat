cdc9e4e06d164883436a134d39b901c0c3cdf2c4
HorusMaster <ramswi@gmail.com>
2024-09-28 17:26:29 -0600

    f

--- cat_video/main.py
+++ cat_video/main.py
@@ -3,6 +3,10 @@ import multiprocessing
 import time
 import torch
 from models.experimental import attempt_load
+import copy
+from utils.datasets import letterbox
+from utils.general import check_img_size
+
 
 def load_model(weights, device):
     model = attempt_load(weights, map_location=device)  # load FP32 model
@@ -12,6 +16,7 @@ def load_model(weights, device):
 def capture_video():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = load_model("models/yolov5n-face.pt", device)
+    img_size = 640
     print("Device is using: ", device)
     # Definir el pipeline GStreamer para utilizar nvarguscamerasrc
     gst_pipeline = (
@@ -22,7 +27,7 @@ def capture_video():
     )
 
     # Abrir la cámara con el pipeline GStreamer
-    cap = cv2.VideoCapture(gst_pipeline, cv2.CAP_GSTREAMER)    
+    cap = cv2.VideoCapture(gst_pipeline, cv2.CAP_GSTREAMER)
 
     # Comprobar si la cámara se abrió correctamente
     if not cap.isOpened():
@@ -39,34 +44,34 @@ def capture_video():
         # Si no se pudo capturar un frame, salir del bucle
         if not ret:
             print("Error: No se pudo recibir el frame.")
-            break
+            break 
 
-        orgimg = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)    
-        # img0 = copy.deepcopy(orgimg)
-        # h0, w0 = orgimg.shape[:2]  # orig hw
-        # r = img_size / max(h0, w0)  # resize image to img_size
-        # if r != 1:  # always resize down, only resize up if training with augmentation
-        #     interp = cv2.INTER_AREA if r < 1  else cv2.INTER_LINEAR
-        #     img0 = cv2.resize(img0, (int(w0 * r), int(h0 * r)), interpolation=interp)
+        orgimg = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        img0 = copy.deepcopy(orgimg)
+        h0, w0 = orgimg.shape[:2]  # orig hw
+        r = img_size / max(h0, w0)  # resize image to img_size
+        if r != 1:  # always resize down, only resize up if training with augmentation
+            interp = cv2.INTER_AREA if r < 1 else cv2.INTER_LINEAR
+            img0 = cv2.resize(img0, (int(w0 * r), int(h0 * r)), interpolation=interp)
 
-        # imgsz = check_img_size(img_size, s=model.stride.max())  # check img_size
+        imgsz = check_img_size(img_size, s=model.stride.max())  # check img_size
 
-        # img = letterbox(img0, new_shape=imgsz)[0]
-        # # Convert from w,h,c to c,w,h
-        # img = img.transpose(2, 0, 1).copy()
+        img = letterbox(img0, new_shape=imgsz)[0]
+        # Convert from w,h,c to c,w,h
+        img = img.transpose(2, 0, 1).copy()
 
-        # img = torch.from_numpy(img).to(device)
-        # img = img.float()  # uint8 to fp16/32
-        # img /= 255.0  # 0 - 255 to 0.0 - 1.0
-        # if img.ndimension() == 3:
-        #     img = img.unsqueeze(0)
+        img = torch.from_numpy(img).to(device)
+        img = img.float()  # uint8 to fp16/32
+        img /= 255.0  # 0 - 255 to 0.0 - 1.0
+        if img.ndimension() == 3:
+            img = img.unsqueeze(0)
 
-        # # Inference
-        # pred = model(img)[0]  
+        # Inference
+        pred = model(img)[0]
         cv2.imshow("Video de la cámara", orgimg)
 
         # Salir del bucle si se presiona la tecla 'q'
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        if cv2.waitKey(1) & 0xFF == ord("q"):
             break
 
     # Liberar la cámara y cerrar las ventanas
@@ -89,6 +94,7 @@ def start_video_capture():
         if process.is_alive():
             process.terminate()
             process.join()
+
 
 if __name__ == "__main__":
     start_video_capture()
