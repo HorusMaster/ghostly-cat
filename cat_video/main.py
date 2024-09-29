06246895aa2d16c49f6be4d5e09ca737b5112c0a
HorusMaster <ramswi@gmail.com>
2024-09-29 00:05:08 +0000

    detect working

--- cat_video/main.py
+++ cat_video/main.py
@@ -5,12 +5,68 @@ import torch
 from models.experimental import attempt_load
 import copy
 from utils.datasets import letterbox
-from utils.general import check_img_size, non_max_suppression_face
+from utils.general import check_img_size, non_max_suppression_face, scale_coords
 
 
 def load_model(weights, device):
     model = attempt_load(weights, map_location=device)  # load FP32 model
     return model
+
+
+def scale_coords_landmarks(img1_shape, coords, img0_shape, ratio_pad=None):
+    # Rescale coords (xyxy) from img1_shape to img0_shape
+    if ratio_pad is None:  # calculate from img0_shape
+        gain = min(
+            img1_shape[0] / img0_shape[0], img1_shape[1] / img0_shape[1]
+        )  # gain  = old / new
+        pad = (img1_shape[1] - img0_shape[1] * gain) / 2, (
+            img1_shape[0] - img0_shape[0] * gain
+        ) / 2  # wh padding
+    else:
+        gain = ratio_pad[0][0]
+        pad = ratio_pad[1]
+
+    coords[:, [0, 2, 4, 6, 8]] -= pad[0]  # x padding
+    coords[:, [1, 3, 5, 7, 9]] -= pad[1]  # y padding
+    coords[:, :10] /= gain
+    # clip_coords(coords, img0_shape)
+    coords[:, 0].clamp_(0, img0_shape[1])  # x1
+    coords[:, 1].clamp_(0, img0_shape[0])  # y1
+    coords[:, 2].clamp_(0, img0_shape[1])  # x2
+    coords[:, 3].clamp_(0, img0_shape[0])  # y2
+    coords[:, 4].clamp_(0, img0_shape[1])  # x3
+    coords[:, 5].clamp_(0, img0_shape[0])  # y3
+    coords[:, 6].clamp_(0, img0_shape[1])  # x4
+    coords[:, 7].clamp_(0, img0_shape[0])  # y4
+    coords[:, 8].clamp_(0, img0_shape[1])  # x5
+    coords[:, 9].clamp_(0, img0_shape[0])  # y5
+    return coords
+
+
+def show_results(img, xyxy, conf, landmarks, class_num):
+    h, w, c = img.shape
+    tl = 1 or round(0.002 * (h + w) / 2) + 1  # line/font thickness
+    x1 = int(xyxy[0])
+    y1 = int(xyxy[1])
+    x2 = int(xyxy[2])
+    y2 = int(xyxy[3])
+    img = img.copy()
+
+    cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), thickness=tl, lineType=cv2.LINE_AA)
+
+    clors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0), (0, 255, 255)]
+
+    for i in range(5):
+        point_x = int(landmarks[2 * i])
+        point_y = int(landmarks[2 * i + 1])
+        cv2.circle(img, (point_x, point_y), tl + 1, clors[i], -1)
+
+    tf = max(tl - 1, 1)  # font thickness
+    label = str(conf)[:5]
+    cv2.putText(
+        img, label, (x1, y1 - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA
+    )
+    return img
 
 
 def capture_video():
@@ -50,6 +106,7 @@ def capture_video():
 
         orgimg = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         img0 = copy.deepcopy(orgimg)
+        im0 = copy.deepcopy(frame)
         h0, w0 = orgimg.shape[:2]  # orig hw
         r = img_size / max(h0, w0)  # resize image to img_size
         if r != 1:  # always resize down, only resize up if training with augmentation
@@ -72,7 +129,29 @@ def capture_video():
         pred = model(img)[0]
         pred = non_max_suppression_face(pred, conf_thres, iou_thres)
         print(len(pred[0]), "face" if len(pred[0]) == 1 else "faces")
-        cv2.imshow("Video de la cámara", orgimg)
+
+        for i, det in enumerate(pred):  # detections per image
+            if len(det):
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+
+                # Print results
+                for c in det[:, -1].unique():
+                    n = (det[:, -1] == c).sum()  # detections per class
+
+                det[:, 5:15] = scale_coords_landmarks(
+                    img.shape[2:], det[:, 5:15], im0.shape
+                ).round()
+
+                for j in range(det.size()[0]):
+                    xyxy = det[j, :4].view(-1).tolist()
+                    conf = det[j, 4].cpu().numpy()
+                    landmarks = det[j, 5:15].view(-1).tolist()
+                    class_num = det[j, 15].cpu().numpy()
+
+                    im0 = show_results(im0, xyxy, conf, landmarks, class_num)
+
+        cv2.imshow("Video de la cámara", im0)
 
         # Salir del bucle si se presiona la tecla 'q'
         if cv2.waitKey(1) & 0xFF == ord("q"):
